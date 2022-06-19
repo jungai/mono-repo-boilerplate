f97c7f05ecfe9f71b4e31910e1d7139307175c3e
jungai <juworaphol125@gmail.com>
2022-06-19 13:54:09 +0700

    refactor: add types from workspace

--- services/express-app/src/controllers/hello/index.ts
+++ services/express-app/src/controllers/hello/index.ts
@@ -1,5 +1,7 @@
+import type { TExample } from "@eiei/types";
 import type { RequestHandler } from "express";
 
 export const helloWorld: RequestHandler = (_req, res, _next) => {
-	res.json({ hello: "world" });
+	const example: TExample = 0;
+	res.json({ hello: "world" + example });
 };
